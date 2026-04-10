8834c6412b4ed6f504734ab704ca6fef23ccb220
matrinezartur--mms <martinezartur@mediamarkt.es>
2026-04-10 19:06:20 +0200

    remove redundant and dummy files

--- problems/energy_2D_fvm/dummy_fluxes.m
+++
@@ -1,7 +0,0 @@
-function [A, b] = dummy_fluxes(w, t, mesh, cp, cv, rho, k, ...
-        speed)
-%DUMMY_FLUXES Summary of this function goes here
-%   Detailed explanation goes here
-    A = zeros(length(mesh.volume));
-    b = zeros(length(mesh.volume), 1);
-end
